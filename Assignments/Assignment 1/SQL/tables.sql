ba4c70a0d8129ab787646b7929f513e8d0f19ff6
kirkbro <maek@itu.dk>
2014-09-17 11:35:01 +0200

    SQL done

--- Assignments/Assignment 1/SQL/tables.sql
+++ Assignments/Assignment 1/SQL/tables.sql
@@ -1,35 +1,4 @@
-CREATE TABLE IF NOT EXISTS Person (
-	id INT AUTO_INCREMENT,
-	name VARCHAR(50) NOT NULL,
-	birthdate DATETIME,
-	deathdate DATETIME,
-	gender BOOLEAN,
-	height INT,
-	PRIMARY KEY (id)
-);
-
-CREATE TABLE IF NOT EXISTS Contract (
-	id INT AUTO_INCREMENT,
-	personId INT NOT NULL,
-	productionId INT NOT NULL,
-	PRIMARY KEY (id),
-	FOREIGN KEY (personId) REFERENCES Person.id,
-	FOREIGN KEY (productionId) REFERENCES Production.id
-);
-
-CREATE TABLE IF NOT EXISTS Actor (
-	contractId INT NOT NULL,
-	characterName VARCHAR(50) NOT NULL,
-	PRIMARY KEY (contractId, characterName),
-	FOREIGN KEY (contractId) REFERENCES Contract.id
-);
-
-CREATE TABLE IF NOT EXISTS Non_Actor (
-	contractId INT NOT NULL,
-	title VARCHAR(50) NOT NULL,
-	PRIMARY KEY (contractId, title),
-	FOREIGN KEY (contractId) REFERENCES Contract.id
-);
+USE filmdatabase;
 
 CREATE TABLE IF NOT EXISTS Production (
 	id INT AUTO_INCREMENT,
@@ -43,12 +12,37 @@ CREATE TABLE IF NOT EXISTS Production (
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS Genre (
-	productionId INT NOT NULL,
-	genreTypeId INT NOT NULL,
-	PRIMARY KEY (productionId, genreTypeId),
-	FOREIGN KEY (productionId) REFERENCES Production.id,
-	FOREIGN KEY (genresId) REFERENCES GenreType.id
+CREATE TABLE IF NOT EXISTS Person (
+	id INT AUTO_INCREMENT,
+	name VARCHAR(50) NOT NULL,
+	birthdate DATETIME,
+	deathdate DATETIME,
+	gender BOOLEAN,
+	height INT,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS Contract (
+	id INT AUTO_INCREMENT,
+	personId INT,
+	productionId INT,
+	PRIMARY KEY (id),
+	FOREIGN KEY (personId) REFERENCES Person(id),
+	FOREIGN KEY (productionId) REFERENCES Production(id)
+);
+
+CREATE TABLE IF NOT EXISTS Actor (
+	contractId INT,
+	characterName VARCHAR(50) NOT NULL,
+	PRIMARY KEY (contractId, characterName),
+	FOREIGN KEY (contractId) REFERENCES Contract(id)
+);
+
+CREATE TABLE IF NOT EXISTS Non_Actor (
+	contractId INT,
+	title VARCHAR(50) NOT NULL,
+	PRIMARY KEY (contractId, title),
+	FOREIGN KEY (contractId) REFERENCES Contract(id)
 );
 
 CREATE TABLE IF NOT EXISTS GenreType (
@@ -57,31 +51,38 @@ CREATE TABLE IF NOT EXISTS GenreType (
 	PRIMARY KEY (id)
 );
 
+CREATE TABLE IF NOT EXISTS Genre (
+	productionId INT,
+	genreTypeId INT,
+	PRIMARY KEY (productionId, genreTypeId),
+	FOREIGN KEY (productionId) REFERENCES Production(id),
+	FOREIGN KEY (genreTypeId) REFERENCES GenreType(id)
+);
 
 CREATE TABLE IF NOT EXISTS Movie (
 	id INT AUTO_INCREMENT,
-	productionId INT NOT NULL,
+	productionId INT,
 	PRIMARY KEY (id),
-	FOREIGN KEY (productionId) REFERENCES Production.id
+	FOREIGN KEY (productionId) REFERENCES Production(id)
 );
 
 CREATE TABLE IF NOT EXISTS Series (
 	id INT AUTO_INCREMENT,
-	productionId INT NOT NULL,
+	productionId INT,
 	PRIMARY KEY (id),
-	FOREIGN KEY (productionId) REFERENCES Production.id
+	FOREIGN KEY (productionId) REFERENCES Production(id)
 );
 
 CREATE TABLE IF NOT EXISTS Season (
 	id INT AUTO_INCREMENT,
-	seriesId INT NOT NULL,
+	seriesId INT,
 	PRIMARY KEY (id),
-	FOREIGN KEY (seriesId) REFERENCES Series.id
+	FOREIGN KEY (seriesId) REFERENCES Series(id)
 );
 
 CREATE TABLE IF NOT EXISTS Episode (
 	id INT AUTO_INCREMENT,
-	seasonId INT NOT NULL,
+	seasonId INT,
 	PRIMARY KEY (id),
-	FOREIGN KEY (seasonId) REFERENCES Season.id
+	FOREIGN KEY (seasonId) REFERENCES Season(id)
 );
