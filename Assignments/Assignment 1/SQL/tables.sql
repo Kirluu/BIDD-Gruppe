74b6b64c0e8fb131c274f2c7f0126a225d432a41
kirkbro <maek@itu.dk>
2014-09-17 11:24:55 +0200

    More work on SQL

--- Assignments/Assignment 1/SQL/tables.sql
+++ Assignments/Assignment 1/SQL/tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Person (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT,
 	name VARCHAR(50) NOT NULL,
 	birthdate DATETIME,
 	deathdate DATETIME,
@@ -9,57 +9,79 @@ CREATE TABLE IF NOT EXISTS Person (
 );
 
 CREATE TABLE IF NOT EXISTS Contract (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT,
 	personId INT NOT NULL,
 	productionId INT NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (personId) REFERENCES Person.id,
+	FOREIGN KEY (productionId) REFERENCES Production.id
 );
 
 CREATE TABLE IF NOT EXISTS Actor (
 	contractId INT NOT NULL,
-	characterName VARCHAR(50),
-	PRIMARY KEY (contractId, characterName)
+	characterName VARCHAR(50) NOT NULL,
+	PRIMARY KEY (contractId, characterName),
+	FOREIGN KEY (contractId) REFERENCES Contract.id
 );
 
 CREATE TABLE IF NOT EXISTS Non_Actor (
 	contractId INT NOT NULL,
-	title VARCHAR(50),
-	PRIMARY KEY (contractId, title)
+	title VARCHAR(50) NOT NULL,
+	PRIMARY KEY (contractId, title),
+	FOREIGN KEY (contractId) REFERENCES Contract.id
 );
 
 CREATE TABLE IF NOT EXISTS Production (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT,
 	length DOUBLE,
-	title VARCHAR(80),
+	title VARCHAR(80) NOT NULL,
 	releaseDate DATETIME,
 	country VARCHAR(50),
 	language VARCHAR(50),
 	imdbRanking DOUBLE,
 	ageRating VARCHAR(4),
-	genre VARCHAR(25),
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS Movie (
-	id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS Genre (
 	productionId INT NOT NULL,
+	genreTypeId INT NOT NULL,
+	PRIMARY KEY (productionId, genreTypeId),
+	FOREIGN KEY (productionId) REFERENCES Production.id,
+	FOREIGN KEY (genresId) REFERENCES GenreType.id
+);
+
+CREATE TABLE IF NOT EXISTS GenreType (
+	id INT AUTO_INCREMENT,
+	genreName VARCHAR(25) NOT NULL,
 	PRIMARY KEY (id)
+);
+
+
+CREATE TABLE IF NOT EXISTS Movie (
+	id INT AUTO_INCREMENT,
+	productionId INT NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (productionId) REFERENCES Production.id
 );
 
 CREATE TABLE IF NOT EXISTS Series (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT,
 	productionId INT NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (productionId) REFERENCES Production.id
 );
 
 CREATE TABLE IF NOT EXISTS Season (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT,
 	seriesId INT NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (seriesId) REFERENCES Series.id
 );
 
 CREATE TABLE IF NOT EXISTS Episode (
-	id INT NOT NULL AUTO_INCREMENT,
+	id INT AUTO_INCREMENT,
 	seasonId INT NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (id),
+	FOREIGN KEY (seasonId) REFERENCES Season.id
 );
