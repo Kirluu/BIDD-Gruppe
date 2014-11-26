e079a110bd0367fe86f979e648e93544d3f53da6
kirkbro <maek@itu.dk>
2014-11-26 11:18:33 +0100

    Removed genregroup bridge and collapsed moviegenre

--- Assignments/Assignment 4/SQL/olap.sql
+++ Assignments/Assignment 4/SQL/olap.sql
@@ -1,49 +1,32 @@
-CREATE TABLE genregroup (
-	groupId INT(11),
-	genreId INT(11),
-    PRIMARY KEY (groupId, genreId),
-	FOREIGN KEY (genreId) REFERENCES genre(id)
-);
-
 CREATE TABLE ratingfacts (
 	movieId INT(11),
 	userId INT(11),
-	genreGroupId INT(11),
+	genreId INT(11),
 	occupationId INT(11),
 	zip CHAR(5) CHARACTER SET latin1 COLLATE latin1_swedish_ci,
 	FOREIGN KEY (movieId) REFERENCES movie(id),
 	FOREIGN KEY (userId) REFERENCES user(id),
-	FOREIGN KEY (genreGroupId) REFERENCES genregroup(groupId),
+	FOREIGN KEY (genreId) REFERENCES genre(id),
 	FOREIGN KEY (occupationId) REFERENCES occupation(id),
 	FOREIGN KEY (zip) REFERENCES zipcode(zip)
 );
 
-INSERT INTO genregroup (
-
-);
-
-SELECT * FROM movie
-JOIN moviegenre ON moviegenre.movieId = movie.id
-GROUP BY movie.id;
-
-
-
 INSERT INTO ratingfacts
-SELECT rating.movieId, rating.userId, genreGroupId, occupation.id as occupation, zipcode.zip
+SELECT rating.movieId, rating.userId, moviegenre.genreId, occupation.id as occupation, zipcode.zip
 FROM rating
 JOIN user ON user.id = rating.userId
+JOIN moviegenre ON moviegenre.movieId = rating.movieId
 JOIN occupation ON occupation.id = user.occupation
-JOIN zipcode ON zipcode.zip = user.zip
-JOIN (SELECT 1 AS genreGroupId) AS s1;
+JOIN zipcode ON zipcode.zip = user.zip;
 
--- Lulz:
-SELECT 1000/pop * COUNT(*) AS ratingsPerPerson, ratingfacts.zip, COUNT(*) AS ratings, pop AS population
+
+SELECT 1000/pop * COUNT(*) ratingsPerThousandPerson, ratingfacts.zip, COUNT(*) ratings, pop population
 FROM ratingfacts
 JOIN zipcodedata ON zipcodedata.zip = ratingfacts.zip
 GROUP BY zipcodedata.zip
-ORDER BY ratingsPerPerson DESC;
+ORDER BY ratingsPerThousandPerson DESC;
 
-SELECT title, rating FROM ratingfacts
-JOIN rating ON rating.movieId = ratingfacts.movieId AND rating.userId = ratingfacts.userId
-JOIN movie ON movie.id = ratingfacts.movieId
-WHERE zip = 10020;
+SELECT 1000/mean * COUNT(*) ratingsPerThousandDollars, ratingfacts.zip, mean meanIncome, COUNT(*) ratings FROM ratingfacts
+JOIN zipcodedata ON zipcodedata.zip = ratingfacts.zip
+GROUP BY ratingfacts.zip
+ORDER BY ratingsPerThousandDollars DESC;
